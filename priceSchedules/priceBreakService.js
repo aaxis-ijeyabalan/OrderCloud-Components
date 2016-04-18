70754a08e3a1f7a72cd6fab98c048fecb543b9fb
Zeeshan Dawood <dawood.zeeshan@gmail.com>
2016-04-18 16:07:52 -0500

    OC-418 update so only unique qauntity values can be entered

--- priceSchedules/priceBreakService.js
+++ priceSchedules/priceBreakService.js
@@ -19,9 +19,13 @@ function PriceBreakFactory (Underscore) {
     }
 
     function addPriceBreak( priceSchedule, price, quantity) {
+        var numberExist= Underscore.findWhere(priceSchedule.PriceBreaks, {Quantity: quantity});
 
-        priceSchedule.PriceBreaks.push({Price: price, Quantity: quantity});
-
+        if(numberExist === undefined){
+            priceSchedule.PriceBreaks.push({Price: price, Quantity: quantity})
+        }else{
+            throw new Error('Quantity already exists, please delete and re-enter quantity to edit','Error');
+        }
         displayQuantity(priceSchedule);
 
         return setMinMax(priceSchedule);
@@ -42,38 +46,39 @@ function PriceBreakFactory (Underscore) {
 
 
     function displayQuantity(priceSchedule){
+
+        //Organize the priceschedule array in order of quantity
         priceSchedule.PriceBreaks.sort(function(a,b){return a.Quantity - b.Quantity});
 
+        //find out the max quantity in the array
+        var maxQuantity = Math.max.apply(Math,priceSchedule.PriceBreaks.map(function(object){
+            return object.Quantity}));
 
-
+        // go through each item in the priceschedule array
         for(var i=0; i < priceSchedule.PriceBreaks.length; i++){
 
-            var maxQuantity = Math.max.apply(Math,priceSchedule.PriceBreaks.map(function(object){
-                return object.Quantity}));
-
-
-            console.log("this is y", y);
-            //if max number and is unique, display max number  with + symbol
+            //if max number is unique, display max number  with + symbol
             if(priceSchedule.PriceBreaks[i].Quantity == maxQuantity) {
                 priceSchedule.PriceBreaks[i].displayQuantity= priceSchedule.PriceBreaks[i].Quantity + "+";
 
             }else{
-                var itemPriceRange = Underscore.range(priceSchedule.PriceBreaks[i].Quantity,priceSchedule.PriceBreaks[i + 1].Quantity);
-                itemPriceRange;
+                //otherwise get the range of numbers between the current index quantity , and the next index Quantity
+                var itemQuantityRange = Underscore.range(priceSchedule.PriceBreaks[i].Quantity, priceSchedule.PriceBreaks[i + 1].Quantity);
 
-                if(itemPriceRange.length === 1){
-                    priceSchedule.PriceBreaks[i].displayQuantity = itemPriceRange[0];
+                itemQuantityRange;
+
+                // If the difference between the range of numbers is only 1 . then just display that quantity number
+                if(itemQuantityRange.length === 1){
+                    priceSchedule.PriceBreaks[i].displayQuantity = itemQuantityRange[0];
                 }else{
-                    //if price range = 0 display one number
-                    if(((priceSchedule.PriceBreaks[priceSchedule.PriceBreaks.length-1]).Quantity - itemPriceRange[0]) <= 1){
-                        priceSchedule.PriceBreaks[i].displayQuantity = itemPriceRange[0];
-                        //displays range
+                    //the last quantity in the array of PriceBreaks minus the 1st quantity in the calculate range is less than or =1 , add only the first number in the Item
+                    if(((priceSchedule.PriceBreaks[priceSchedule.PriceBreaks.length-1]).Quantity - itemQuantityRange[0]) <= 1){
+                        priceSchedule.PriceBreaks[i].displayQuantity = itemQuantityRange[0];
+                        //displays range between two quantities in the array
                     }else{
-                        priceSchedule.PriceBreaks[i].displayQuantity = itemPriceRange[0] + "-" + itemPriceRange[itemPriceRange.length-1] ;
-                        var something;
+                        priceSchedule.PriceBreaks[i].displayQuantity = itemQuantityRange[0] + "-" + itemQuantityRange[itemQuantityRange.length-1] ;
                     }
                 }
-
 
             }
         }
