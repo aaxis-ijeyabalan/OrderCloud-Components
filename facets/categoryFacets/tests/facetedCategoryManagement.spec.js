882bbd10f08b24693ab9e7ff2c8b45ba9bbaac60
Miranda Posthumus <mposthumus@four51.com>
2016-05-03 08:41:21 -0500

    OC-465 facet unit test updates

--- facets/categoryFacets/tests/facetedCategoryManagement.spec.js
+++ facets/categoryFacets/tests/facetedCategoryManagement.spec.js
@@ -1,7 +1,8 @@
-fdescribe('Component: Category Facets', function() {
+describe('Component: Category Facets', function() {
    var scope,
        q,
        category,
+       matchingProds,
        oc;
     beforeEach(module('orderCloud'));
     beforeEach(module('orderCloud.sdk'));
@@ -26,6 +27,22 @@ fdescribe('Component: Category Facets', function() {
                     }
                 }
             }
+        };
+        matchingProds = {
+          Meta: {},
+            Items: [
+                {
+                    ID: 'prod123465789',
+                    xp: {
+                        OC_Facets: {
+                            TestCategory123456789: {
+                                color: ['blue']
+                            }
+                        }
+                    }
+                }
+            ]
+
         };
         oc = OrderCloud;
     }));
@@ -53,25 +70,88 @@ fdescribe('Component: Category Facets', function() {
     });
     describe('Controller: FacetedCategoryManageController', function() {
         var facetedCategoryManageCtrl;
-        var newFacetValue = 'purple';
         beforeEach(inject(function($controller){
             facetedCategoryManageCtrl = $controller('CategoryFacetsManageCtrl', {
                 $scope: scope,
-                Category: category,
-                facetName: 'color'
+                Category: category
             });
+            facetedCategoryManageCtrl.facetName = 'color'
         }));
         describe('addValueExisting', function(){
             beforeEach(function() {
                 var defer = q.defer();
                 defer.resolve(category);
+                facetedCategoryManageCtrl.color = {
+                    newFacetValue: 'Purple'
+                };
                 spyOn(oc.Categories, 'Update').and.returnValue(defer.promise);
                 facetedCategoryManageCtrl.addValueExisting(facetedCategoryManageCtrl.facetName, 1);
             });
-            it('should call the Categories Update method', function(){
-                category.xp.OC_Facets.color.Values.push(newFacetValue);
+            it('should call the Categories Update method and add purple to the values', function(){
                 expect(oc.Categories.Update).toHaveBeenCalledWith(category.ID, category);
+                expect(category.xp.OC_Facets.color.Values[3]).toEqual('purple');
+                expect(category.xp.OC_Facets.color.Values.length).toEqual(4);
             });
+        });
+        describe('removeValueExisting', function(){
+            beforeEach(function() {
+                spyOn(oc.Categories, 'Update').and.returnValue(null);
+                facetedCategoryManageCtrl.removeValueExisting(facetedCategoryManageCtrl.facetName, 3);
+            });
+            it('should call the Categories Update method and remove blue from the values', function(){
+                category.xp.OC_Facets.color.Values.splice(2, 1);
+                expect(oc.Categories.Update).toHaveBeenCalledWith(category.ID, category);
+                expect(category.xp.OC_Facets.color.Values.indexOf('blue')).toEqual(-1);
+                expect(category.xp.OC_Facets.color.Values.length).toEqual(2);
+            });
+        });
+        describe('toggleFacetRequired', function(){
+            beforeEach(function() {
+                spyOn(oc.Categories, 'Update').and.returnValue(null);
+                facetedCategoryManageCtrl.toggleFacetRequired(facetedCategoryManageCtrl.facetName);
+            });
+            it('should call the Categories Update method and toggle isRequired', function(){
+                expect(oc.Categories.Update).toHaveBeenCalledWith(category.ID, category);
+                expect(category.xp.OC_Facets.color.isRequired).toEqual(true);
+                facetedCategoryManageCtrl.toggleFacetRequired(facetedCategoryManageCtrl.facetName);
+                expect(oc.Categories.Update).toHaveBeenCalledWith(category.ID, category);
+                expect(category.xp.OC_Facets.color.isRequired).toEqual(false);
+            });
+        });
+        describe('deleteFacet', function(){
+           beforeEach(function(){
+               var defer = q.defer();
+               defer.resolve();
+               var proddfd = q.defer();
+               proddfd.resolve(matchingProds);
+               spyOn(oc.Categories, 'Update').and.returnValue(defer.promise);
+               spyOn(oc.Products, 'List').and.returnValue(proddfd.promise);
+               spyOn(oc.Products, 'Update').and.returnValue(null);
+               spyOn(window, 'confirm').and.returnValue(true);
+               facetedCategoryManageCtrl.deleteFacet(facetedCategoryManageCtrl.facetName);
+           });
+            it('should call the Categories Update method', function(){
+                delete category.xp.OC_Facets;
+                expect(oc.Categories.Update).toHaveBeenCalledWith(category.ID, category);
+                scope.$digest();
+                //var keyName = {
+                //    xp: {
+                //        OC_Facets: {
+                //            TestCategory123456789: {
+                //                color: ['blue']
+                //            }
+                //        }
+                //    }
+                //};
+                var filterObj = 'xp.OC_Facets.TestCategory123456789.color: *';
+
+                expect(oc.Products.List).toHaveBeenCalledWith(null, 1, 100, null, null, filterObj);
+                delete matchingProds.Items[0].xp.OC_Facets.TestCategory123456789;
+                expect(oc.Products.Update).toHaveBeenCalledWith(matchingProds.ID, matchingProds)
+            });
+            //it('should call the Products List method', function(){
+            //    expect(oc.Categories.Update).toHaveBeenCalledWith(category.ID, category);
+            //});
         });
     });
 });
