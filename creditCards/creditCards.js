a6924c5c3dd921bd9fe76a33c0e62a525bd3d19f
Zeeshan Dawood <dawood.zeeshan@gmail.com>
2016-06-13 09:14:51 -0500

    OC-916 seperate views for the credit card list and credit card assignments

--- creditCards/creditCards.js
+++ creditCards/creditCards.js
@@ -14,14 +14,30 @@ function CreditCardsConfig( $stateProvider ) {
     $stateProvider
         .state( 'creditCards', {
             parent: 'base',
-            url: '/creditCards',
-            templateUrl:'creditCards/templates/creditCards.tpl.html',
-            controller:'CreditCardsCtrl',
-            controllerAs: 'creditCards',
+            views: {
+                '': {
+                    templateUrl:'creditCards/templates/creditCards.tpl.html',
+                    controller:'CreditCardsCtrl',
+                    controllerAs: 'creditCards'
+                },
+                'filters@creditCards': {
+                    templateUrl:'creditCards/templates/creditCard.filters.tpl.html'
+                },
+                'list@creditCards': {
+                    templateUrl:'creditCards/templates/creditCard.list.tpl.html'
+                }
+            },
+            url: '/credit-cards?from&to&search&page&pageSize&searchOn&sortBy&filters',
             data: {componentName: 'Credit Cards'},
             resolve: {
-                CreditCardList: function(OrderCloud) {
-                    return OrderCloud.CreditCards.List();
+                // UserType: function(OrderCloud) {
+                //     return JSON.parse(atob(OrderCloud.Auth.ReadToken().split('.')[1])).usrtype;
+                // },
+                Parameters: function( $stateParams, OrderCloudParameters ) {
+                    return OrderCloudParameters.Get($stateParams);
+                },
+                CreditCardList: function( OrderCloud, Parameters ) {
+                    return OrderCloud.CreditCards.List(Parameters.search, Parameters.page, Parameters.pageSize || 12, Parameters.searchOn, Parameters.sortBy);
                 }
             }
         })
@@ -43,6 +59,20 @@ function CreditCardsConfig( $stateProvider ) {
             controllerAs: 'creditCardCreate'
         })
         .state( 'creditCards.assign', {
+            views: {
+                '': {
+                    templateUrl:'creditCards/templates/creditCardAssign.tpl.html',
+                    controller:'CreditCardAssignCtrl',
+                    controllerAs: 'creditCardAssign'
+                },
+                'filters@creditCards.assign': {
+                    templateUrl:'creditCards/templates/creditCardAssign.filters.tpl.html'
+                },
+                'list@creditCards.assign': {
+                    templateUrl:'creditCards/templates/creditCardAssign.list.tpl.html'
+                }
+            },
+           /* url: '/credit-cards?from&to&search&page&pageSize&searchOn&sortBy&filters',*/
             url: '/:creditCardid/assign',
             templateUrl: 'creditCards/templates/creditCardAssign.tpl.html',
             controller: 'CreditCardAssignCtrl',
@@ -57,16 +87,92 @@ function CreditCardsConfig( $stateProvider ) {
                 AssignedUserGroups: function($stateParams, OrderCloud) {
                     return OrderCloud.CreditCards.ListAssignments($stateParams.creditCardid);
                 },
+                Parameters: function( $stateParams, OrderCloudParameters ) {
+                    return OrderCloudParameters.Get($stateParams);
+                },
                 SelectedCreditCard: function($stateParams, OrderCloud) {
                     return OrderCloud.CreditCards.Get($stateParams.creditCardid);
                 }
+                // CreditCardList: function( OrderCloud, Parameters ) {
+                //     return OrderCloud.CreditCards.List(Parameters.search, Parameters.page, Parameters.pageSize || 12, Parameters.searchOn, Parameters.sortBy);
+                // }
             }
         })
 }
 
-function CreditCardsController( CreditCardList ) {
+function CreditCardsController( $state, $ocMedia, OrderCloud, CreditCardList, Parameters, OrderCloudParameters ) {
     var vm = this;
     vm.list = CreditCardList;
+    vm.parameters = Parameters;
+    vm.sortSelection = Parameters.sortBy ? (Parameters.sortBy.indexOf('!') == 0 ? Parameters.sortBy.split('!')[1] : Parameters.sortBy) : null;
+
+    //Check if filters are applied
+    vm.filtersApplied = vm.parameters.filters || vm.parameters.from || vm.parameters.to || ($ocMedia('max-width:767px') && vm.sortSelection); //Sort by is a filter on mobile devices
+    vm.showFilters = vm.filtersApplied;
+
+    //Check if search was used
+    vm.searchResults = Parameters.search && Parameters.search.length > 0;
+
+    //Reload the state with new parameters
+    vm.filter = function(resetPage) {
+        $state.go('.', OrderCloudParameters.Create(vm.parameters, resetPage));
+    };
+
+    //Reload the state with new search parameter & reset the page
+    vm.search = function() {
+        vm.filter(true);
+    };
+
+    //Clear the search parameter, reload the state & reset the page
+    vm.clearSearch = function() {
+        vm.parameters.search = null;
+        vm.filter(true);
+    };
+
+    //Clear relevant filters, reload the state & reset the page
+    vm.clearFilters = function() {
+        vm.parameters.filters = null;
+        vm.parameters.from = null;
+        vm.parameters.to = null;
+        $ocMedia('max-width:767px') ? vm.parameters.sortBy = null : angular.noop(); //Clear out sort by on mobile devices
+        vm.filter(true);
+    };
+
+    //Conditionally set, reverse, remove the sortBy parameter & reload the state
+    vm.updateSort = function(value) {
+        value ? angular.noop() : value = vm.sortSelection;
+        switch(vm.parameters.sortBy) {
+            case value:
+                vm.parameters.sortBy = '!' + value;
+                break;
+            case '!' + value:
+                vm.parameters.sortBy = null;
+                break;
+            default:
+                vm.parameters.sortBy = value;
+        }
+        vm.filter(false);
+    };
+
+    //Used on mobile devices
+    vm.reverseSort = function() {
+        Parameters.sortBy.indexOf('!') == 0 ? vm.parameters.sortBy = Parameters.sortBy.split('!')[1] : vm.parameters.sortBy = '!' + Parameters.sortBy;
+        vm.filter(false);
+    };
+
+    //Reload the state with the incremented page parameter
+    vm.pageChanged = function() {
+        $state.go('.', {page:vm.list.Meta.Page});
+    };
+
+    //Load the next page of results with all of the same parameters
+    vm.loadMore = function() {
+        return OrderCloud.CreditCards.List(Parameters.search, vm.list.Meta.Page + 1, Parameters.pageSize ||  vm.list.Meta.PageSize, Parameters.searchOn, Parameters.sortBy, Parameters.filters)
+            .then(function(data) {
+                vm.list.Items = vm.list.Items.concat(data.Items);
+                vm.list.Meta = data.Meta;
+            });
+    };
 }
 
 function creditCardExpirationDate(){
