1fc4d12a98cbdf4346a0385670f24d39772dafa7
Zeeshan Dawood <dawood.zeeshan@gmail.com>
2016-06-14 14:20:29 -0500

    OC-918 Acceptance Criteria *In mobile view card view appears instead of tables *In mobile view load more button should work - no pagination *In mobile view filters button shows up- # sortby options should work # Once an option is selected, a button to sort by should appear and will sort by that selected function # Should be an X button that appears adjacent to the filter button. Once clicked  it should clear the filterad *when selecting the cog icon drop down shows up and links work *Desktop view should have pagination that works * Search Filter should work , x button will appear, when clicked it should clear search * If table header is blue then it should sortby that header. A carrot should appear to indicate alphabetical order. It goes away when the sortby is no longer applied.

--- giftCards/giftCards.js
+++ giftCards/giftCards.js
@@ -14,15 +14,31 @@ function GiftCardsConfig( $stateProvider ) {
     $stateProvider
         .state( 'giftCards', {
             parent: 'base',
-            url: '/giftCards',
-            templateUrl: 'giftCards/templates/giftCards.tpl.html',
-            controller: 'GiftCardsCtrl',
-            controllerAs: 'giftCards',
+            views: {
+                '': {
+                    templateUrl:'giftCards/templates/giftCards.tpl.html',
+                    controller:'GiftCardsCtrl',
+                    controllerAs: 'giftCards'
+                },
+                'filters@giftCards': {
+                    templateUrl:'giftCards/templates/giftCards.filters.tpl.html'
+                },
+                'list@giftCards': {
+                    templateUrl:'giftCards/templates/giftCards.list.tpl.html'
+                }
+            },
+            url: '/giftCards?search&page&pageSize&searchOn&sortBy&filters',
             data: {componentName: 'Gift Cards'},
             resolve: {
-                GiftCardList: function(OrderCloud) {
-                    return OrderCloud.SpendingAccounts.List(null, null, null, null, null, {'RedemptionCode': '*'});
-                }
+
+                Parameters: function( $stateParams, OrderCloudParameters ) {
+                    return OrderCloudParameters.Get($stateParams);
+                },
+                GiftCardList: function(OrderCloud,Parameters) {
+                    var parameters = angular.copy(Parameters);
+                    parameters.filters ? parameters.filters.RedemptionCode = '*': parameters.filters = {RedemptionCode :'*'};
+                    return OrderCloud.SpendingAccounts.List(parameters.search, parameters.page, parameters.pageSize || 12, parameters.searchOn, parameters.sortBy, parameters.filters);
+                    }
             }
         })
         .state( 'giftCards.edit', {
@@ -89,9 +105,11 @@ function GiftCardsConfig( $stateProvider ) {
         });
 }
 
-function GiftCardsController ( OrderCloud, GiftCardList, TrackSearch ) {
+function GiftCardsController ( $state, $ocMedia, OrderCloud, GiftCardList, TrackSearch,OrderCloudParameters, Parameters ) {
     var vm = this;
     vm.list = GiftCardList;
+    vm.parameters = Parameters;
+    vm.sortSelection = Parameters.sortBy ? (Parameters.sortBy.indexOf('!') == 0 ? Parameters.sortBy.split('!')[1] : Parameters.sortBy) : null;
     vm.pagingfunction = PagingFunction;
     vm.searchfunction = Search;
     vm.searching = function() {
@@ -107,6 +125,78 @@ function GiftCardsController ( OrderCloud, GiftCardList, TrackSearch ) {
     function Search(searchTerm) {
         return OrderCloud.SpendingAccounts.List(searchTerm, null, null, null, null, {'RedemptionCode': '*'});
     }
+
+    //Check if filters are applied
+    vm.filtersApplied = vm.parameters.filters || vm.parameters.from || vm.parameters.to || ($ocMedia('max-width:767px') && vm.sortSelection); //Sort by is a filter on mobile devices
+    vm.showFilters = vm.filtersApplied;
+
+    //Check if search was used
+    vm.searchResults = Parameters.search && Parameters.search.length > 0;
+
+    //Reload the state with new parameters
+    vm.filter = function(resetPage) {
+        $state.go('.', OrderCloudParameters.Create(vm.parameters, resetPage));
+    };
+
+    //Reload the state with new search parameter & reset the page
+    vm.search = function() {
+        vm.filter(true);
+    };
+
+    //Clear the search parameter, reload the state & reset the page
+    vm.clearSearch = function() {
+        vm.parameters.search = null;
+        vm.filter(true);
+    };
+
+    //Clear relevant filters, reload the state & reset the page
+    vm.clearFilters = function() {
+        vm.parameters.filters = null;
+        vm.parameters.from = null;
+        vm.parameters.to = null;
+        $ocMedia('max-width:767px') ? vm.parameters.sortBy = null : angular.noop(); //Clear out sort by on mobile devices
+        vm.filter(true);
+    };
+
+    //Conditionally set, reverse, remove the sortBy parameter & reload the state
+    vm.updateSort = function(value) {
+        value ? angular.noop() : value = vm.sortSelection;
+        switch(vm.parameters.sortBy) {
+            case value:
+                vm.parameters.sortBy = '!' + value;
+                break;
+            case '!' + value:
+                vm.parameters.sortBy = null;
+                break;
+            default:
+                vm.parameters.sortBy = value;
+        }
+        vm.filter(false);
+    };
+
+    //Used on mobile devices
+    vm.reverseSort = function() {
+        Parameters.sortBy.indexOf('!') == 0 ? vm.parameters.sortBy = Parameters.sortBy.split('!')[1] : vm.parameters.sortBy = '!' + Parameters.sortBy;
+        vm.filter(false);
+    };
+
+    //Reload the state with the incremented page parameter
+    vm.pageChanged = function() {
+        $state.go('.', {page:vm.list.Meta.Page});
+    };
+
+    //Load the next page of results with all of the same parameters
+    vm.loadMore = function() {
+        var parameters = angular.copy(Parameters);
+        parameters.filters ? parameters.filters.RedemptionCode = '*': parameters.filters = {RedemptionCode :'*'};
+        return OrderCloud.SpendingAccounts.List(parameters.search, vm.list.Meta.Page + 1, parameters.pageSize ||  vm.list.Meta.PageSize, parameters.searchOn, parameters.sortBy, parameters.filters)
+        // return OrderCloud.Orders[UserType == 'admin' ? 'ListIncoming' : 'ListOutgoing'](Parameters.from, Parameters.to, Parameters.search, vm.list.Meta.Page + 1, Parameters.pageSize || vm.list.Meta.PageSize, Parameters.searchOn, Parameters.sortBy, Parameters.filters)
+            .then(function(data) {
+                vm.list.Items = vm.list.Items.concat(data.Items);
+                vm.list.Meta = data.Meta;
+            });
+    };
+
 }
 
 function GiftCardEditController($state, $exceptionHandler, OrderCloud, SelectedGiftCard, GiftCardFactory, toastr) {
