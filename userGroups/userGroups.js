b286fb6541adc02a31a68167cad95818ba42567b
Robert <rwatt@four51.com>
2016-06-14 10:51:43 -0600

    OC-924

--- userGroups/userGroups.js
+++ userGroups/userGroups.js
@@ -13,14 +13,27 @@ function UserGroupsConfig( $stateProvider ) {
     $stateProvider
         .state( 'userGroups', {
             parent: 'base',
-            url: '/userGroups',
-            templateUrl:'userGroups/templates/userGroups.tpl.html',
-            controller:'UserGroupsCtrl',
-            controllerAs: 'userGroups',
+            views: {
+                '': {
+                    templateUrl:'userGroups/templates/userGroups.tpl.html',
+                    controller:'UserGroupsCtrl',
+                    controllerAs: 'userGroups'
+                },
+                'filters@userGroups': {
+                    templateUrl:'userGroups/templates/userGroups.filters.tpl.html'
+                },
+                'list@userGroups': {
+                    templateUrl:'userGroups/templates/userGroups.list.tpl.html'
+                }
+            },
+            url: '/userGroups?search&page&pageSize&sortBy&searchOn&filters',
             data: {componentName: 'User Groups'},
             resolve: {
-                UserGroupList: function(OrderCloud) {
-                    return OrderCloud.UserGroups.List(null, 1, 20);
+                Parameters: function($stateParams, OrderCloudParameters) {
+                    return OrderCloudParameters.Get($stateParams);
+                },
+                UserGroupList: function(OrderCloud, Parameters) {
+                    return OrderCloud.UserGroups.List(Parameters.search, Parameters.page, Parameters.pageSize || 12, Parameters.searchOn, Parameters.sortBy, Parameters.filters);
                 }
             }
         })
@@ -60,11 +73,77 @@ function UserGroupsConfig( $stateProvider ) {
         })
 }
 
-function UserGroupsController( UserGroupList, TrackSearch ) {
+function UserGroupsController( $state, $ocMedia, OrderCloud, UserGroupList, Parameters, OrderCloudParameters ) {
     var vm = this;
     vm.list = UserGroupList;
-    vm.searching = function() {
-        return TrackSearch.GetTerm() ? true : false;
+    vm.parameters = Parameters;
+    vm.sortSelection = Parameters.sortBy ? (Parameters.sortBy.indexOf('!') == 0 ? Parameters.sortBy.split('!')[1] : Parameters.sortBy) : null;
+
+
+    //Check if filters are applied
+    vm.filtersApplied = vm.parameters.filters || ($ocMedia('max-width:767px') && vm.sortSelection); //Sort by is a filter on mobile devices
+    vm.showFilters = vm.filtersApplied;
+
+    //Check if search was used
+    vm.searchResults = Parameters.search && Parameters.search.length > 0;
+
+    //Reload the state with new parameters
+    vm.filter = function(resetPage) {
+        $state.go('.', OrderCloudParameters.Create(vm.parameters, resetPage));
+    };
+
+    //Reload the state with new search parameter & reset the page
+    vm.search = function() {
+        vm.filter(true);
+    };
+
+    //Clear the search parameter, reload the state & reset the page
+    vm.clearSearch = function() {
+        vm.parameters.search = null;
+        vm.filter(true);
+    };
+
+    //Clear relevant filters, reload the state & reset the page
+    vm.clearFilters = function() {
+        vm.parameters.filters = null;
+        $ocMedia('max-width:767px') ? vm.parameters.sortBy = null : angular.noop(); //Clear out sort by on mobile devices
+        vm.filter(true);
+    };
+
+    //Conditionally set, reverse, remove the sortBy parameter & reload the state
+    vm.updateSort = function(value) {
+        value ? angular.noop() : value = vm.sortSelection;
+        switch(vm.parameters.sortBy) {
+            case value:
+                vm.parameters.sortBy = '!' + value;
+                break;
+            case '!' + value:
+                vm.parameters.sortBy = null;
+                break;
+            default:
+                vm.parameters.sortBy = value;
+        }
+        vm.filter(false);
+    };
+
+    //Used on mobile devices
+    vm.reverseSort = function() {
+        Parameters.sortBy.indexOf('!') == 0 ? vm.parameters.sortBy = Parameters.sortBy.split('!')[1] : vm.parameters.sortBy = '!' + Parameters.sortBy;
+        vm.filter(false);
+    };
+
+    //Reload the state with the incremented page parameter
+    vm.pageChanged = function() {
+        $state.go('.', {page:vm.list.Meta.Page});
+    };
+
+    //Load the next page of results with all of the same parameters
+    vm.loadMore = function() {
+        return OrderCloud.UserGroups.List( Parameters.search, vm.list.Meta.Page + 1, Parameters.pageSize || vm.list.Meta.PageSize, Parameters.searchOn, Parameters.sortBy, Parameters.filters)
+            .then(function(data) {
+                vm.list.Items = vm.list.Items.concat(data.Items);
+                vm.list.Meta = data.Meta;
+            });
     };
 }
 
